2b02d984ffc6bb10f753ff6851b06bcad1df4e40
Leandro <contato.leandrogoncalves@gmail.com>
2016-09-01 21:38:58 -0300

    change folder base

--- Projeto_Banco_de_dados_2sis/Aluno.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE POO_Usuario
-(
-  id    number(5) constraint pk_user primary key,
-  login varchar(100) constraint nn_usu_login not null,
-  senha varchar(100) 
-);
-
-INSERT INTO POO_Usuario VALUES (1,'Leandro', '75179');
